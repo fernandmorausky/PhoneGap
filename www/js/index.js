d25b9301e2145c49694b26c1005e7ae3a8b723a0
fernando <fernandmorausky@gmail.com>
2016-04-22 15:18:34 -0500

    q

--- www/js/index.js
+++ www/js/index.js
@@ -38,19 +38,23 @@ var app = {
                 app.receivedEvent('deviceready');
                 var ref = window.open('http://ingenieriadeseguridad.telefonica.com/es/portada.html', '_blank', 'location=no, toolbar=no');
                 ref.addEventListener('loadstart', function(event) { 
-                      if (!(navigator.onLine)) {
-                        alert('No cuenta con internet');
-                        ref.close();
-                        return;
-                      }
+                      // if ((navigator.onLine)) {
+                      //   alert('No cuenta con internet');
+                      //   ref.close();
+                      //   return;
+                      // }
                  });
                 //ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
                 //ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
-                //ref.addEventListener('offline', function(event) { alert('No cuenta con internet'); } );
+                ref.addEventListener('offline', function(event) { 
+                    alert('No cuenta con internet');
+                    ref.close();
+                    return;
+                } );
                 //ref.addEventListener('exit', function(event) { alert(event.type); });
             } 
             else{
-                //alert('No cuenta con internet');
+                alert('No cuenta con internet');
             }
 
         }catch(e){
